332bd24ee4363a8d45aa982bd255ceb9a16febe7
gitkim <106027169+adcgunwoo@users.noreply.github.com>
2024-12-08 16:28:06 +0900

    feat : C++ 입문(주석 설명) 및 문장 출력
    유튜브 하루10분 코딩의 C++설명을 통해 처음 배우는 부분은 주석 처리로 설명

---
+++ C++문제제출용/소스1.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //전처리 지시자, 헤더파일
+/*
+c++에서 함수를 사용하고자 한다면
+반드시 그 함수의 원형을 미리 정의하여야 한다.
+*/
+using namespace std;
+//위 iostream에서 cout와 endl과 같은 함수를 사용하려면
+//미리 선언하거나 일일히 앞에 붙여줘야함
+int main()
+{
+	cout << "Fun Programming" << endl;
+	//cout는 printf, endl은 엔터
+	//<<는 데이터의 흐름, 방향을 나타냄
+	return 0;
+}
