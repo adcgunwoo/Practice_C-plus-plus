8ff5860ef6c1107a9a46e0da466b68df13984e0b
gitkim <106027169+adcgunwoo@users.noreply.github.com>
2024-12-09 14:55:23 +0900

    feat : 입력 및 출력
    입력받을때는 cin >> 변수 로 입력받는다, 서식 문자가 따로 필요없고, 꺽새 방향이 반대이다.

---
+++ C++문제제출용/소스3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, j;
+	cout << "첫 번째 숫자 입력 : ";
+	cin >> i;
+	cout << "두 번째 숫자 입력 : ";
+	cin >> j;
+	cout <<"숫자의 합은 = " << i + j;
+	return 0;
+}
